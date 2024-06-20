da2fd7286951fd0a508183bd1676d3297037e326
ksh-wtag <shakawat.ratul@welldev.io>
2024-06-20 17:49:44 +0600

    Add Forecast Database

---
+++ WeatherApp/Models/DabaseModels/ForecastDatabaseModel.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastDatabaseModel.swift
+//  WeatherApp
+//
+//  Created by Kazi Shakawat Hossain Ratul on 20/6/24.
+//
+
+import Foundation
---
+++ WeatherApp/Services/ForecastDatabaseOperation.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastDatabaseOperation.swift
+//  WeatherApp
+//
+//  Created by Kazi Shakawat Hossain Ratul on 20/6/24.
+//
+
+import Foundation
